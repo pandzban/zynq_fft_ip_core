12cf0912a55585097d173f510ceec49dd3a57d0d
pandzban <ag96pl@gmail.com>
2021-04-15 20:49:38 +0200

    add module_sim project base

---
+++ module_sim/fft_module/fft_module.srcs/sim_1/new/butterfly_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.04.2021 20:38:47
+// Design Name: 
+// Module Name: butterfly_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module butterfly_tb(
+
+    );
+endmodule
---
+++ module_sim/fft_module/fft_module.srcs/sources_1/new/butterfly_beh.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 15.04.2021 20:37:17
+// Design Name: 
+// Module Name: butterfly_beh
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module butterfly_beh(
+
+    );
+endmodule
